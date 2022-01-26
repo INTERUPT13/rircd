160ee1946a61d30565ffc02b1d68d85647234850
Your Name <you@example.com>
2022-01-26 11:22:38 +0100

    introduced start phase + logging

--- src/endpoint.rs
+++ src/endpoint.rs
@@ -2,19 +2,40 @@ use tokio::sync::{RwLock, mpsc};
 use std::sync::Arc;
 use crate::event::Event;
 use crate::channel::Channel;
+use color_eyre::Result;
+use log::{debug,info};
+
+use async_trait::async_trait;
 
 // trait that is being fullfilled by the actual protocol specific Endpoints
 // like Irc Telegram ....
+#[async_trait]
 trait EndpointBackend {
+    async fn start(self: Box<Self>) -> Result<()>;
 }
 
-struct Endpoint {
+impl Endpoint {
+    // TODO it should be possible to just mut borrow the endpoint
+    // since server shouldn't terminate before self.run() or stuff
+    pub async fn start(self) -> Result<()> {
+        debug!("trying to start endpoint {}", self.name());
+        self.backend.start().await
+    }
+
+
+    fn name(&self) -> String {
+        self.name.clone()
+    }
+}
+
+pub struct Endpoint {
     // we track the channels an endpoint is part of so that in case the
     // endpoint gets removed/goes offline we can  first remove it from
     // the channels it is part of. To prevent messages being delivered
     // into nowhere
     in_channels: Vec<Arc<Channel>>,
     backend: Box<dyn EndpointBackend>,
+    name: String,
 }
 
 // changing internal values of endpoints isn't performed through directly
--- src/server.rs
+++ src/server.rs
@@ -1,16 +1,55 @@
 use std::sync::Arc;
+use color_eyre::{eyre::eyre, Result};
+use futures::StreamExt;
+use crate::endpoint::{Endpoint, EndpointContact};
+use futures::stream::FuturesUnordered;
 
-use crate::endpoint::EndpointContact;
+use log::{info,debug,trace};
 
+struct ServerConfig {
+    start_min_endpoints: usize,
+}
 
-impl Server {
-    pub fn new() -> Server {
-        Server {
-            endpoints: Vec::new(),
+impl  Default for ServerConfig {
+    fn default() -> Self {
+        Self {
+            start_min_endpoints: 1,
         }
     }
 }
 
+impl Server {
+    pub fn new() -> Server {
+        Server {
+            config: ServerConfig::default(),
+            endpoints: Vec::new(),
+        }
+    }
+
+    async fn try_start_all_endpoints(&self, endpoints_to_start: Vec<Endpoint>) -> Vec<Result<()>>  {
+        // TODO is there a better way?
+        let results: FuturesUnordered<_> = endpoints_to_start.into_iter()
+            .map(|ep| ep.start()).collect();
+        results.collect().await
+    }
+
+    //pub fn start_endpoint(&self);
+
+    pub async fn run(&mut self, endpoints_to_start: Vec<Endpoint>) -> Result<()> {
+        info!("starting server");
+        {
+            debug!("bringing up endpoints");
+            let endpoints_status  = self.try_start_all_endpoints(endpoints_to_start).await;
+            let endpoints_success: Vec<_> = endpoints_status.iter().filter(|ep| ep.is_ok()).collect();
+            if endpoints_success.len() < self.config.start_min_endpoints {
+                return Err(eyre!("less than min({}) endpoints started. Aborting",self.config.start_min_endpoints))
+            }
+        }
+        Ok(())
+    }
+}
+
 pub struct Server {
+    config: ServerConfig,
     endpoints: Vec<Arc<EndpointContact>>,
 }
