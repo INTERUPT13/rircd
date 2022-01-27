5bce52db8d9b4dda14551c930c3f1f9dfb260d60
Your Name <you@example.com>
2022-01-27 12:02:30 +0100

    inital commit

--- src/message.rs
+++
@@ -1,6 +0,0 @@
-
-
-// TODO impl Display since {:?} is ugly
-#[derive(Debug)]
-pub struct Message {
-}
--- src/shell/mod.rs
+++
@@ -1,2 +0,0 @@
-// stdin shell input endpoint 
-//      (runnig this in a tmux and having a shell seems quite cool doesn't it)
